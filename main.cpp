f4b731551b4a3f621d8af5d1afe5379e7b4b1a70
zubrowka <takase@zeze.jp>
2014-10-31 14:24:30 +0900

    github push test

---
+++ main.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <time.h>
+#include <stdio.h>
+
+using namespace std;
+
+void draw(char in);
+
+int x, y;
+
+int main(int argc, char** argv){
+    
+    x = 0;
+    y = 0;
+
+    char inp;
+    inp = getchar();
+    while(true){
+        draw(inp);
+
+        cin >> inp;
+        cout << endl;
+    }
+    
+    return 0;
+}
+
+
+void draw(char in){
+    cout << "\033[2J";
+    if(in == 'h') x--;
+    if(in == 'l') x++;
+    if(in == 'j') y++;
+    if(in == 'k') y--;
+
+
+    cout << "\033[" << y << ";" << x << "H";
+    cout << "*" << endl;
+    cout << "\033[10;1H" << " MOVE(hjkl): ";
+}
